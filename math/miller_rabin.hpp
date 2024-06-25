e5788b6c1e655a1b258b590c967096dab3035f6f
downerkei <r.okano0122@gmail.com>
2024-06-25 11:57:36 +0900

    minor fix

--- math/miller_rabin.hpp
+++ math/miller_rabin.hpp
@@ -1,12 +1,12 @@
 #include "../data_structure/montgomery_modint_64.hpp"
 
 namespace fast_factorize {
-    
+
 using mint = MontgomeryModint64;
 bool miller_rabin(long long N, vector<long long> A) {
     mint::set_mod(N);
     long long s = 0, d = N - 1;
-    while(d % 2 == 0) {
+    while(!(d & 1)) {
         s++;
         d >>= 1;
     }
@@ -27,10 +27,11 @@ bool miller_rabin(long long N, vector<long long> A) {
 bool is_prime(long long N) {
     if(N <= 1) return false;
     if(N == 2) return true;
-    if(N % 2 == 0) return false;
+    if(!(N & 1)) return false;
 
     if(N < 4759123141LL) return miller_rabin(N, {2, 7, 61});
 
     return miller_rabin(N, {2, 325, 9375, 28178, 450775, 9780504, 1795265022});
 }
+
 } // namespace fast_factorize
