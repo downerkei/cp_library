3b09ffa71969ef39a7ca1696fbbf3f0d65cbb076
downerkei <r.okano0122@gmail.com>
2024-08-22 21:06:18 +0900

    verify lca

---
+++ verify/yukicoder/yukicoder_1094.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1094"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../graph/tree/lowest_common_ancestor.hpp"
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    vector<vector<pair<int, int>>> G(N);
+    for(int i = 0; i < N - 1; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        a--; b--;
+        G[a].push_back({b, c});
+        G[b].push_back({a, c});
+    }
+
+    LowestCommonAncestor<int> lca(G);
+
+    int Q;
+    cin >> Q;
+    while(Q--) {
+        int s, t;
+        cin >> s >> t;
+        s--; t--;
+        cout << lca.dist_bitween(s, t) << "\n";
+    }
+
+    return 0;
+}
