7e97339046a93e6be8810930fff9c0d4a8630fab
downerkei <r.okano0122@gmail.com>
2024-12-01 17:46:49 +0900

    fix sa

--- string/suffix_array.hpp
+++ string/suffix_array.hpp
@@ -2,27 +2,37 @@ vector<int> suffix_array(const string& S) {
     int N = S.size();
     vector<int> rank(N), sa(N);
 
-    // 1文字のランク計算
     for(int i = 0; i < N; i++) {
         sa[i] = i;
-        rank[i] = S[i] - 'a';
+        rank[i] = S[i];
     }
 
     for(int k = 1; k < N; k *= 2) {
-        // rankに基づいてソート
-        auto cmp = [&](int i, int j) {
-            if(rank[i] != rank[j]) return rank[i] < rank[j];
-            int ri = i + k < N ? rank[i + k] : -1;
-            int rj = j + k < N ? rank[j + k] : -1;
-            return ri < rj;
-        };
-        sort(sa.begin(), sa.end(), cmp);
+        auto get_rank = [&](int i) -> int { return (i < N) ? rank[i] : 0; };
 
-        // 次のrank計算
+        auto radix_sort = [&](int k) -> void {
+            const int CHAR_SIZE = 130;
+            vector<int> backet(max(N + 1, CHAR_SIZE), 0), nsa(N);
+            for(int i = 0; i < N; i++) {
+                backet[get_rank(i + k)]++;
+            }
+            for(int i = 1; i < backet.size(); i++) {
+                backet[i] += backet[i - 1];
+            }
+            for(int i = N; i--;) {
+                int& x = backet[get_rank(sa[i] + k)];
+                nsa[--x] = sa[i];
+            }
+            swap(sa, nsa);
+        };
+        radix_sort(k);
+        radix_sort(0);
+
         vector<int> nrank(N);
-        nrank[sa[0]] = 0;
+        nrank[sa[0]] = 1;
         for(int i = 1; i < N; i++) {
-            nrank[sa[i]] = nrank[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
+            bool is_different = (get_rank(sa[i - 1]) != get_rank(sa[i]) || get_rank(sa[i - 1] + k) != get_rank(sa[i] + k));
+            nrank[sa[i]] = nrank[sa[i - 1]] + is_different;
         }
         swap(rank, nrank);
     }
