be487c563be97f8c2a86f11385fc8ac6292ed189
downerkei <r.okano0122@gmail.com>
2025-02-01 03:08:12 +0900

    fix geometry

--- geometry/geometry.hpp
+++ geometry/geometry.hpp
@@ -36,38 +36,19 @@ struct Point {
     constexpr Point rot(Real theta) const { return Point(x * cos(theta) - y * sin(theta), x * sin(theta) + y * cos(theta)); }
     constexpr Point rot90() const { return Point(-y, x); }
 
-    friend istream& operator >> (istream& is, Point& p) { return is >> p.x >> p.y; }
-    friend ostream& operator << (ostream& os, const Point& p) { return os << "(" << p.x << ", " << p.y << ")"; }
-
-    friend constexpr Real dot(const Point& a, const Point& b) { return a.x * b.x + a.y * b.y; }
-    friend constexpr Real cross(const Point& a, const Point& b) { return a.x * b.y - a.y * b.x; }
-
     friend constexpr Real norm2(const Point& p) { return p.norm2(); }
     friend constexpr Real norm(const Point& p) { return p.norm(); }
     friend constexpr Real arg(const Point& p) { return p.arg(); }
     friend constexpr Point rot(const Point& p, Real theta) { return p.rot(theta); }
     friend constexpr Point rot90(const Point& p) { return p.rot90(); }
+
+    friend istream& operator >> (istream& is, Point& p) { return is >> p.x >> p.y; }
+    friend ostream& operator << (ostream& os, const Point& p) { return os << "(" << p.x << ", " << p.y << ")"; }
 };
 
-struct Line {
-    Point s, t;
-    constexpr Line(Point s=Point(0, 0), Point t=Point(0, 0)) : s(s), t(t) {}
-    constexpr Point dir() const { return t - s; }
-    constexpr Real norm() const { return dir().norm(); }
-
-    friend constexpr Point dir(const Line& l) { return l.dir(); }
-    friend constexpr Real norm(const Line& l) { return l.norm(); }
-};
-
-Point projection(const Line& l, const Point& p) {
-    Real t = dot(p - l.s, l.t - l.s) / norm2(l.t - l.s);
-    return l.s + l.dir() * t;
-}
-
-Point reflection(const Line& l, const Point& p) {
-    Point x = projection(l, p);
-    return 2 * x - p;
-}
+constexpr Real dot(const Point& a, const Point& b) { return a.x * b.x + a.y * b.y; }
+constexpr Real cross(const Point& a, const Point& b) { return a.x * b.y - a.y * b.x; }
+constexpr Point polar(Real r, Real theta) { return Point(r * cos(theta), r * sin(theta)); }
 
 // +1 : a - b, a - c : ccw
 // -1 : a - b, a - c : cw
@@ -83,28 +64,38 @@ int ccw(const Point& a, Point b, Point c) {
     return 0; 
 }
 
-bool is_parallel(const Line& a, const Line& b) {
-    return sgn(cross(a.dir(), b.dir())) == 0;
+struct Line {
+    Point s, t;
+    constexpr Line(Point s=Point(0, 0), Point t=Point(0, 0)) : s(s), t(t) {}
+    constexpr Point dir() const { return t - s; }
+    constexpr Real norm() const { return dir().norm(); }
+    constexpr Point normalize() const { return (t - s) / norm(); }
+
+    friend constexpr Point dir(const Line& l) { return l.dir(); }
+    friend constexpr Real norm(const Line& l) { return l.norm(); }
+    friend constexpr Point normalize(const Line& l) { return l.normalize(); }
+};
+
+Point projection(const Line& l, const Point& p) {
+    Real t = dot(p - l.s, l.t - l.s) / norm2(l.t - l.s);
+    return l.s + l.dir() * t;
+}
+Point reflection(const Line& l, const Point& p) {
+    Point x = projection(l, p);
+    return 2 * x - p;
 }
 
-bool is_orthogonal(const Line& a, const Line& b) {
-    return sgn(dot(a.dir(), b.dir())) == 0;
-}
+bool is_parallel(const Line& a, const Line& b) { return sgn(cross(a.dir(), b.dir())) == 0; }
+bool is_orthogonal(const Line& a, const Line& b) { return sgn(dot(a.dir(), b.dir())) == 0; }
+bool is_intersect(const Line& a, const Line& b) { return ccw(a.s, a.t, b.s) * ccw(a.s, a.t, b.t) <= 0 && ccw(b.s, b.t, a.s) * ccw(b.s, b.t, a.t) <= 0; }
 
-bool is_intersect(const Line& a, const Line& b) {
-    return ccw(a.s, a.t, b.s) * ccw(a.s, a.t, b.t) <= 0 && ccw(b.s, b.t, a.s) * ccw(b.s, b.t, a.t) <= 0;
-}
-
-Point cross_point(const Line& a, const Line& b) {
+Point cross_ll(const Line& a, const Line& b) {
     Real d1 = cross(a.dir(), b.dir());
     Real d2 = cross(a.dir(), a.t - b.s);
     return b.s + b.dir() * (d2 / d1);
 }
 
-Real dist2(const Point& a, const Point& b) {
-    return norm2(a - b);
-}
-Real dist(const Point& a, const Point& b) {
+Real dist_pp(const Point& a, const Point& b) {
     return norm(a - b);
 }
 Real dist_lp(const Line& l, const Point& p) {
@@ -139,21 +130,7 @@ struct Polygon : public vector<Point> {
         return true;
     }
 
-    // 2 : contain
-    // 1 : on line
-    // 0 : outside
-    int contain(const Point& p) const {
-        bool in = false;
-        int sz = size();
-        for(int i = 0; i < sz; i++) {
-            Point a = (*this)[i] - p;
-            Point b = (*this)[(i + 1) % sz] - p;
-            if(a.y > b.y) std::swap(a, b);
-            if(sgn(a.y) == -1 && sgn(b.y) == 1 && sgn(cross(a, b)) == 1) in ^= 1;
-            if(sgn(cross(a, b)) == -1 && sgn(dot(a, b)) == -1) return 1;
-        }
-        return 2 * in;
-    }
+    
 
     pair<int, int> diameter() const {
         assert(is_convex());
@@ -174,9 +151,24 @@ struct Polygon : public vector<Point> {
 
     friend Real area(const Polygon& pol) { return pol.area(); }
     friend bool is_convex(const Polygon& pol) { return pol.is_convex(); }
-    friend int contain(const Polygon& pol, const Point& p) { return pol.contain(p); }
     friend pair<int, int> diameter(const Polygon& pol) { return pol.diameter(); } 
 };
+
+// 2 : contain
+// 1 : on line
+// 0 : outside
+int contain(const Polygon& pol, const Point& p) {
+    bool in = false;
+    int sz = pol.size();
+    for(int i = 0; i < sz; i++) {
+        Point a = pol[i] - p;
+        Point b = pol[(i + 1) % sz] - p;
+        if(a.y > b.y) swap(a, b);
+        if(sgn(a.y) == -1 && sgn(b.y) == 1 && sgn(cross(a, b)) == 1) in ^= 1;
+        if(sgn(cross(a, b)) == -1 && sgn(dot(a, b)) == -1) return 1;
+    }
+    return 2 * in;
+}
 
 Polygon convex_hull(Polygon pol) {
     int sz = pol.size();
@@ -201,7 +193,7 @@ Polygon convex_cut(const Polygon& pol, const Line& l) {
     for(int i = 0; i < pol.size(); i++) {
         int t1 = sgn(cross(l.dir(), pol[i] - l.s)), t2 = sgn(cross(l.dir(), pol[(i + 1) % sz] - l.s));
         if(t1 >= 0) ret.push_back(pol[i]); 
-        if(t1 * t2 < 0) ret.push_back(cross_point(Line(pol[i], pol[(i + 1) % sz]), l));
+        if(t1 * t2 < 0) ret.push_back(cross_ll(Line(pol[i], pol[(i + 1) % sz]), l));
     }
     return ret;
 }
@@ -220,7 +212,7 @@ Real closest_pair(vector<Point> ps) {
             int sz = near_line.size();
             for(int j = sz; j--;) {
                 if(ps[i].y - near_line[j].y >= d) break;
-                d = min(d, dist(ps[i], near_line[j]));
+                d = min(d, dist_pp(ps[i], near_line[j]));
             }
             near_line.push_back(ps[i]);
         }
@@ -235,21 +227,31 @@ struct Circle {
     Real r;
     Circle() : p(Point(0, 0)), r(0) {}
     Circle(Point p, Real r) : p(p), r(r) {}
-
-    int intersection(const Circle& c) {
-        Real d = dist(p, c.p);
-        if(sgn(d - (r + c.r)) == 1) return 4;
-        if(sgn(d - (r + c.r)) == 0) return 3;
-        if(sgn(d - abs(r - c.r)) == 1) return 2;
-        if(sgn(d - abs(r - c.r)) == 0) return 1;
-        return 0;
-    }
 };
 
+int intersection(const Circle& c1, const Circle& c2) {
+    Real d = dist_pp(c1.p, c2.p);
+    if(sgn(d - (c1.r + c2.r)) == 1) return 4;
+    if(sgn(d - (c1.r + c2.r)) == 0) return 3;
+    if(sgn(d - abs(c1.r - c2.r)) == 1) return 2;
+    if(sgn(d - abs(c1.r - c2.r)) == 0) return 1;
+    return 0;
+}
+
+pair<Point, Point> cross_cl(const Circle& c, const Line& l) {
+    Point proj = projection(l, c.p);
+    Real h = dist_lp(l, c.p);
+    Real d = sqrt(c.r * c.r - h * h);
+    Point p1 = proj + l.normalize() * d;
+    Point p2 = proj - l.normalize() * d;
+    if(p1 > p2) swap(p1, p2);
+    return {p1, p2};
+}
+
 Circle incircle(const Point& a, const Point& b, const Point& c) {
-    Real d1 = dist(b, c);
-    Real d2 = dist(c, a);
-    Real d3 = dist(a, b);
+    Real d1 = dist_pp(b, c);
+    Real d2 = dist_pp(c, a);
+    Real d3 = dist_pp(a, b);
     Point ip = (a * d1 + b * d2 + c * d3) / (d1 + d2 + d3);
     Real r = dist_lp(Line(a, b), ip);
     return Circle(ip, r);
@@ -258,9 +260,19 @@ Circle incircle(const Point& a, const Point& b, const Point& c) {
 Circle outcircle(const Point& a, const Point& b, const Point& c) {
     Line l1((a + b) / 2, (a + b) / 2 + rot90(b - a));
     Line l2((b + c) / 2, (b + c) / 2 + rot90(c - b));
-    Point op = cross_point(l1, l2);
-    Real r = dist(op, a);
+    Point op = cross_ll(l1, l2);
+    Real r = dist_pp(op, a);
     return Circle(op, r);
+}
+
+pair<Point, Point> cross_cc(const Circle& c1, const Circle& c2) {
+    Real d = dist_pp(c1.p, c2.p);
+    Real theta = acos((c1.r * c1.r + d * d - c2.r * c2.r) / (2 * c1.r * d));
+    Real phi = arg(c2.p - c1.p);
+    Point p1 = c1.p + polar(c1.r, phi - theta);
+    Point p2 = c1.p + polar(c1.r, phi + theta);
+    if(p1 > p2) swap(p1, p2);
+    return {p1, p2};
 }
 
 }  // namespace geometry
