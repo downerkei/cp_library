50194d0ab7d4e5678cb1f7a529f2098e635551f5
downerkei <r.okano0122@gmail.com>
2025-02-11 15:51:07 +0900

    fix fenwick_tree

--- data_structure/fenwick_tree.hpp
+++ data_structure/fenwick_tree.hpp
@@ -1,19 +1,17 @@
-template <typename T>
+template <typename T=int>
 struct FenwickTree {
-    int n_;
+    int n;
     vector<T> data;
-    FenwickTree(int n) : n_(n), data(n, 0) {}
+    FenwickTree(int n) : n(n), data(n, 0) {}
 
-    // data[p] += x
     void add(int p, T x) {
         p++;
-        while(p <= n_) {
+        while(p <= n) {
             data[p - 1] += x;
             p += p & -p;
         }
     }
 
-    // sum[0, r)
     T sum(int r) {
         T s = 0;
         while(r > 0) {
@@ -23,7 +21,6 @@ struct FenwickTree {
         return s;
     }
     
-    // sum[l, r)
     T sum(int l, int r) {
         return sum(r) - sum(l);
     }
