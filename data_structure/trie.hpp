26f818e0428db17f7b49a1cebd6251f464784edb
downerkei <r.okano0122@gmail.com>
2025-02-16 02:17:05 +0900

    create trie

---
+++ data_structure/trie.hpp
@@ -0,0 +1,31 @@
+template <int ALPHA_SIZE=26>
+struct Trie {
+    struct Node {
+        vector<Node*> children;
+        bool is_end;
+        Node() : is_end(false), children(ALPHA_SIZE, nullptr) {}
+    };
+
+    Node* root;
+
+    void insert(const string& word) {
+        Node* cur_node = root;
+        for(char c : word) {
+            int idx = c - 'a';
+            if(cur_node->children[idx] == nullptr) cur_node->children[idx] = Node();
+            cur_node = cur_node->children[idx];
+        }
+
+        cur_node->is_end = true;
+    }
+
+    bool search(const string& word) {
+        Node* cur_node = root;
+        for(char c : word) {
+            int idx = c - 'a';
+            if(cur_node->children[idx] == nullptr) return false;
+            cur_node = cur_node->children[idx];
+        }
+        return cur_node->is_end;
+    }
+};
