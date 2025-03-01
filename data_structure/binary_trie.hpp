c13fc593348d6ea11f24bb0de68cdbea4146af64
downerkei <r.okano0122@gmail.com>
2025-03-01 16:41:21 +0900

    fix binary_trie

--- data_structure/binary_trie.hpp
+++ data_structure/binary_trie.hpp
@@ -6,42 +6,39 @@ struct BinaryTrie {
         Node() : cnt(0), ch{nullptr, nullptr} {}
     };
 
-    BinaryTrie() {}
+    BinaryTrie() : root(nullptr) {}
 
-    void insert(U val) { root = insert(root, val); }
-    void erase(U val) { root = erase(root, val); }
-    bool find(U val) { return find(root, val); }
-    U xor_max(U val) { return xor_min(root, ~val); }
-    U xor_min(U val) { return xor_min(root, val); }
+    void insert(U x) { insert(root, x); }
+    void erase(U x) { erase(root, x); }
+    bool find(U x) { return find(root, x); }
+    U xor_max(U x) { return xor_min(root, ~x); }
+    U xor_min(U x) { return xor_min(root, x); }
 
-  private: 
+//   private: 
     Node* root;
 
-    Node* insert(Node* n, U val, int b = B - 1) {
+    void insert(Node*& n, U x, int b = B - 1) {
         if(n == nullptr) n = new Node();
         n->cnt++;
-        if(b >= 0) n->ch[(val >> b) & 1] = insert(n->ch[(val >> b) & 1], val, b - 1);
-        return n;
+        if(b >= 0) insert(n->ch[(x >> b) & 1], x, b - 1);
+    }    
+
+    void erase(Node*& n, U x, int b = B - 1) {
+        if(b >= 0) erase(n->ch[(x >> b) & 1], x, b - 1);
+        if(n && --n->cnt == 0) { delete n; n = nullptr; }
     }
 
-    Node* erase(Node* n, U val, int b = B - 1) {
-        if(b >= 0) n->ch[(val >> b) & 1] = erase(n->ch[(val >> b) & 1], val, b - 1);
-        n->cnt--;
-        if(n->cnt == 0) { delete n; return nullptr; }
-        return n;
-    }
-
-    bool find(Node* n, U val, int b = B - 1) {
+    bool find(Node* n, U x, int b = B - 1) {
+        if(n == nullptr) return false;
         if(b < 0) return true;
-        int f = (val >> b) & 1;
-        if(n->ch[f] == nullptr) return false;
-        return find(n->ch[f], val, b - 1);
+        if(n->ch[(x >> b) & 1] == nullptr) return false;
+        return find(n->ch[(x >> b) & 1], x, b - 1);
     }
 
-    U xor_min(Node* n, U val, int b = B - 1) {
+    U xor_min(Node* n, U x, int b = B - 1) {
         if(b < 0) return 0;
-        bool f = (val >> b) & 1;
+        bool f = (x >> b) & 1;
         if(n->ch[f] == nullptr) f ^= 1;
-        return xor_min(n->ch[f], val, b - 1) | ((U)f << b);
+        return xor_min(n->ch[f], x, b - 1) | ((U)f << b);
     }
 };
