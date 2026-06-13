27362ca542a11cebe94e7730789d11da3766fe5f
downerkei <r.okano0122@gmail.com>
2026-06-14 02:25:56 +0900

    fix parser

--- string/parser.hpp
+++ string/parser.hpp
@@ -3,40 +3,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 構文解析ひな型
 struct Parser {
-    string s;
-    int pos = 0;
+    string S;
+    int N, it = 0;
 
-    Parser(const string& s) : s(s) {}
+    Parser(const string& S) : S(S), N(S.size()) {}
 
-    bool eof() const {
-        return pos >= (int)s.size();
+    // 読み終わりを判定する．
+    bool eof() const {  
+        return it >= N;
     }
 
+    // 今の文字を返す，itは進めない．
     char peek() const {
-        if (eof()) return '\0';
-        return s[pos];
+        return eof() ? '\0' : S[it];
     }
 
+    // あれば読む，boolを返す．
     bool accept(char c) {
         if (peek() == c) {
-            pos++;
+            it++;
             return true;
         }
         return false;
     }
 
+    // 必ず読む，assertかかる．
     void expect(char c) {
         assert(accept(c));
     }
 
+    // 数字を返す，intに注意．
     int number() {
         assert(isdigit(peek()));
 
         int val = 0;
         while (!eof() && isdigit(peek())) {
             val = val * 10 + (peek() - '0');
-            pos++;
+            it++;
         }
         return val;
     }
