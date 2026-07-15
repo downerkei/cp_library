9351f346bf66d8e29ed77587f6993821e85d5206
downerkei <r.okano0122@gmail.com>
2026-07-16 01:29:38 +0900

    fix ext_gcd

--- math/ext_gcd.hpp
+++ math/ext_gcd.hpp
@@ -1,4 +1,4 @@
-tuple<long long, long long, long long> ext_gcd(long long a, long long b) {
+array<long long, 3> ext_gcd(long long a, long long b) {
     if(b == 0) return {a, 1, 0};
     auto [d, y, x] = ext_gcd(b, a % b);
     y -= a / b * x;
